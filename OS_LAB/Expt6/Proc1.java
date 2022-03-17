ac9a0ae0ee1cf4683f04c2da861a6e0210294c03
Amal <amalthundiyil0@gmail.com>
2022-03-17 15:53:56 +0530

    expt6_os

--- OS_LAB/Expt6/Proc1.java
+++ OS_LAB/Expt6/Proc1.java
@@ -12,15 +12,42 @@ public class Proc1 {
         MappedByteBuffer mem = fc.map(FileChannel.MapMode.READ_WRITE, 0, 1000);
 
         try {
-            Thread.sleep(10000);
+            Thread.sleep(100);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        String example = "Convert Java String";
+        byte[] bytes = example.getBytes();
 
-        for (int i = 1; i < 1000; i++) {
-            mem.put((byte) i);
-            System.out.println("Process 1 : " + (byte) i);
+        for (byte b : bytes) {
+            mem.put(b);
             Thread.sleep(1);
         }
     }
+}
+
+
+  public class MemoryMappedFileInJava {
+
+    private static int count = 10485760; 
+
+    public static void main(String[] args) throws Exception {
+
+        RandomAccessFile memoryMappedFile = new RandomAccessFile("largeFile.txt", "rw");
+        MappedByteBuffer out = memoryMappedFile.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, count);
+
+        for (int i = 0; i < count; i++) {
+            out.put((byte) 'A');
+        }
+
+        System.out.println("Writing to Memory Mapped File is completed");
+
+        for (int i = 0; i < 10 ; i++) {
+            System.out.print((char) out.get(i));
+        }
+
+        System.out.println("Reading from Memory Mapped File is completed");
+
+    }
+
 }
