4e5de0627cebccb66db42519fca41457a8223a31
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-03-23 20:59:14 +0530

    Semaphore started

---
+++ OS_LAB/Expt7/p.c
@@ -0,0 +1,22 @@
+#include <error.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+
+int main() {
+    int choice = 1;
+    char input[20];
+    key_t key = ftok("shmfile", 65);
+    int segment_id = shmget(key, 4096, (40 * sizeof(int)) | IPC_CREAT);
+    char* shared_memory = (char*)shmat(segment_id, NULL, 0);
+    printf("Shared memory data: %s\n", shared_memory);
+    printf("Enter the state: \n");
+    scanf("%s", input);
+    sprintf(shared_memory, input);
+    shmdt(shared_memory);
+    return 0;
+}
