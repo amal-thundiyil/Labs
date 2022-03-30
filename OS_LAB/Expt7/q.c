0e715f01fc4293bb90b2695b48d0d3fa35ee7c95
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-03-30 18:38:22 +0530

    Semaphores Init

--- OS_LAB/Expt7/q.c
+++ OS_LAB/Expt7/q.c
@@ -1,26 +1,43 @@
-#include <error.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-int main() {
-    int choice = 1;
-    char input[20];
-    key_t key = ftok("shmfile", 65);
-    int segment_id = shmget(key, 4096, (40 * sizeof(int)) | IPC_CREAT);
-    char* shared_memory = (char*)shmat(segment_id, NULL, 0);
-    while (choice == 1) {
-        printf("Shared memory data: %s\n", shared_memory);
-        printf("Enter the state: \n");
-        scanf("%s", input);
-        sprintf(shared_memory, input);
-        printf("Press 1 to continue: \n");
-        scanf("%d", &choice);
+#define SEM_MUTEX_NAME "/sem-mutex"
+#define MAX_BUFFERS 40
+#define COUNT -1
+
+char buf[MAX_BUFFERS][100];
+int buffer_index;
+int buffer_print_index;
+
+sem_t *mutex_sem, *buffer_count_sem, *mutex_sem;
+
+int main(void) {
+    if ((mutex_sem = sem_open(SEM_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
+        perror("sem_open");
+        exit(1);
     }
-    shmdt(shared_memory);
-    return 0;
+
+    int semval;
+    while (1) {
+        if (sem_getvalue(mutex_sem, &semval) == -1)
+            perror("sem_getvalue");
+        if (!semval) break;
+        sleep(1);
+    }
+
+    if (sem_unlink("COUNT") == -1) {
+        perror("sem_unlink");
+        exit(1);
+    }
+
+    exit(0);
 }
--- OS_LAB/Expt7/r.c
+++ OS_LAB/Expt7/r.c
@@ -1,45 +1,45 @@
 // The program r.c initializes n number of semaphores. It first assign count equal -1, which is then used by process p and q. This count is protected by semaphore. It also allocates shared memory of size 40 ints. It waits for process p and q to enter all n1 and n2 elements through different terminals. This program r.c sorts shared data in ascending order. It waits to finish p and q. At end, The program r.c detaches and deletes n semaphores and print the sum of all elements of the list.
-#include <error.h>
+
+#include <errno.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
-sem_t mutex;
+#define SEM_MUTEX_NAME "/sem-mutex"
+#define MAX_BUFFERS 40
+#define COUNT -1
 
-void* thread(void* arg) {
-    sem_wait(&mutex);
-    printf("\nEntered..\n");
+char buf[MAX_BUFFERS][100];
+int buffer_index;
+int buffer_print_index;
 
-    sleep(4);
+sem_t *mutex_sem, *buffer_count_sem, *mutex_sem;
 
-    printf("\nJust Exiting...\n");
-    sem_post(&mutex);
-}
+int main(void) {
+    if ((mutex_sem = sem_open(SEM_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
+        perror("sem_open");
+        exit(1);
+    }
 
-int main() {
-    int choice = 1;
-    int n = 4, count = -1;
-    key_t key = ftok("shmfile", 65);
-    int segment_id = shmget(key, 4096, (40 * sizeof(int)) | IPC_CREAT);
-    char* shared_memory = (char*)shmat(segment_id, NULL, 0);
-    sprintf(shared_memory, "%d", count);
-    printf("Shared memory data: %s\n", shared_memory);
+    int semval;
+    while (1) {
+        if (sem_getvalue(mutex_sem, &semval) == -1)
+            perror("sem_getvalue");
+        if (!semval) break;
+        sleep(1);
+    }
 
-    // sem_init(&mutex, 0, 1);
-    // pthread_t t1, t2;
-    // pthread_create(&t1, NULL, thread, NULL);
-    // sleep(2);
-    // pthread_create(&t2, NULL, thread, NULL);
-    // pthread_join(t1, NULL);
-    // pthread_join(t2, NULL);
+    if (sem_unlink("COUNT") == -1) {
+        perror("sem_unlink");
+        exit(1);
+    }
 
-    shmdt(shared_memory);
-    sem_destroy(&mutex);
-    return 0;
+    exit(0);
 }
