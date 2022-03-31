5d0036432f8fc0c8df15521b2d57ba761603b650
Amal Thundiyil <amalthundiyil0@gmail.com>
2022-03-31 07:23:35 +0530

    Semaphore Insertion sort

--- OS_LAB/Expt7/p.c
+++ OS_LAB/Expt7/p.c
@@ -1,43 +1,23 @@
-#include <errno.h>
-#include <fcntl.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <time.h>
-#include <unistd.h>
 
-#define SEM_MUTEX_NAME "/sem-mutex"
-#define MAX_BUFFERS 40
-#define COUNT -1
-
-char buf[MAX_BUFFERS][100];
-int buffer_index;
-int buffer_print_index;
-
-sem_t *mutex_sem, *buffer_count_sem, *mutex_sem;
-
-int main(void) {
-    if ((mutex_sem = sem_open(SEM_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
-        perror("sem_open");
-        exit(1);
-    }
-
-    int semval;
-    while (1) {
-        if (sem_getvalue(mutex_sem, &semval) == -1)
-            perror("sem_getvalue");
-        if (!semval) break;
-        sleep(1);
-    }
-
-    if (sem_unlink("COUNT") == -1) {
-        perror("sem_unlink");
-        exit(1);
-    }
-
-    exit(0);
+int main() {
+    int n;
+    printf("Enter no. of elems: \n");
+    scanf("%d", &n);
+    printf("\n");
+    key_t key = 1234;
+    int sh_id = shmget(key, 40 * sizeof(int), IPC_CREAT | 0777);
+    int *sh = (int *)shmat(sh_id, NULL, 0);
+    sem_t *p = sem_open("r", 0);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &sh[i]);
+    sh[n] = -1;
+    sem_post(p);
 }
--- OS_LAB/Expt7/r.c
+++ OS_LAB/Expt7/r.c
@@ -1,60 +1,56 @@
-// The program r.c initializes n number of semaphores. It first assign count equal -1, which is then used by process p and q. This count is protected by semaphore. It also allocates shared memory of size 40 ints. It waits for process p and q to enter all n1 and n2 elements through different terminals. This program r.c sorts shared data in ascending order. It waits to finish p and q. At end, The program r.c detaches and deletes n semaphores and print the sum of all elements of the list.
-
-#include <errno.h>
+#include <error.h>
 #include <fcntl.h>
-#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/shm.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <time.h>
 #include <unistd.h>
+#define SHMSZ 27
 
-#define SEM_MUTEX_NAME "/sem-mutex"
-#define MAX_BUFFERS 40
-#define COUNT -1
-
-char buf[MAX_BUFFERS][100];
-int buffer_index;
-int buffer_print_index;
-
-sem_t *mutex_sem, *buffer_count_sem, *mutex_sem;
-
-int main(void) {
-    // if ((mutex_sem = sem_open(SEM_MUTEX_NAME, O_CREAT, 0660, 1)) == SEM_FAILED) {
-    //     perror("sem_open");
-    //     exit(1);
-    // }
-
-    key_t key = ftok("shared_file", 65);
-    int input;
-    int segment_id = shmget(key, (MAX_BUFFERS + 1) * sizeof(int), 0666 | IPC_CREAT);
-    int* shared_memory = (int*)shmat(segment_id, NULL, 0);
-    printf("Shared memory: ");
-    for (int i = 0; i < MAX_BUFFERS; i++) {
-        printf("%d ", *(shared_memory + i));
+void insertion_sort(int arr[], int n) {
+    int i, j;
+    for (int i = 0; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (arr[j] >= key && j >= 0) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
     }
-    // printf("Enter the state: \n");
-    // scanf("%s", input);
-    // sprintf(shared_memory, input);
-    shmdt(shared_memory);
-
-    // int semval;
-    // while (1) {
-    //     if (sem_getvalue(mutex_sem, &semval) == -1)
-    //         perror("sem_getvalue");
-    //     if (!semval) break;
-    //     sleep(1);
-    // }
-
-    // if (sem_unlink("COUNT") == -1) {
-    //     perror("sem_unlink");
-    //     exit(1);
-    // }
-
-    exit(0);
+}
+int main() {
+    key_t key = 1234;
+    sem_unlink("r");
+    sem_t *r = sem_open("r", O_CREAT | O_EXCL, 0660, 0);
+    if (r == SEM_FAILED) {
+        perror("ERROR !! \n");
+        exit(EXIT_FAILURE);
+    }
+    int sh_id = shmget(key, 40 * sizeof(int), IPC_CREAT | 0777);
+    int *sh = (int *)shmat(sh_id, NULL, 0);
+    sem_wait(r);
+    sem_post(r);
+    sleep(2);
+    sem_wait(r);
+    int c = 0;
+    while (1) {
+        if (sh[c] == -1) {
+            break;
+        } else
+            c++;
+    }
+    insertion_sort(sh, c);
+    printf("Final sorted data: \n");
+    for (int i = 0; i < c; i++)
+        printf("%d ", sh[i]);
+    printf("\n");
+    int ans = 0;
+    for (int i = 0; i < 40; i++) {
+        ans += sh[i];
+    }
+    printf("Sum: %d", ans);
+    sem_close(r);
 }
